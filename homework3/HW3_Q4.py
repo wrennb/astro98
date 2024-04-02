8aa7834bc2f7ac4f9cef973e276c890d60b9a102
wrennb <157635221+wrennb@users.noreply.github.com>
2024-04-01 22:13:04 -0700

    Delete homework3 directory

--- homework3/HW3_Q4.py
+++
@@ -1,9 +0,0 @@
-def calculate_bmi(weight, height):
-    bmi = weight / (height ** 2)
-    return bmi
-
-# Testing code:
-weight = 72  # kilograms
-height = 2  # meters
-bmi = calculate_bmi(weight, height)
-print("This BMI is:", bmi)
